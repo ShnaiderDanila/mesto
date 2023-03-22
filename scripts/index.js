c7729d5fe3430e34026ea3fa0d82b2f9362027f3
Shnaider Danila <shnd3r@yandex.ru>
2023-03-22 15:40:23 +0700

    fix: Исправить функции в JS и убрать лишние переменные

--- scripts/index.js
+++ scripts/index.js
@@ -5,41 +5,36 @@ const profileTitle = profile.querySelector('.profile__title');
 const profileSubtitle = profile.querySelector('.profile__subtitle');
 // Elements variables
 const elements = document.querySelector('.elements');
-const cardButtons = elements.querySelectorAll('.elements__card-button');
+const cardButtons = elements.querySelectorAll('.card__button');
 // Popup variables
 const popup = document.querySelector('.popup');
 const buttonClose = popup.querySelector('.popup__button-close');
-const buttonSave = popup.querySelector('.popup__button-save');
-const popupForm = popup.querySelector('.popup__container');
+const popupForm = popup.querySelector('.popup__form');
 const nameInput = popup.querySelector('.popup__input_type_name');
 const jobInput = popup.querySelector('.popup__input_type_job');
 
 // Elements functions
 cardButtons.forEach(function(cardButton) {
-  cardButton.addEventListener('mouseover', function() {
-    cardButton.classList.toggle('elements__card-button_type_hover');
-  });
-  cardButton.addEventListener('mouseout', function() {
-    cardButton.classList.toggle('elements__card-button_type_hover');
-  });
   cardButton.addEventListener('click', function() {
-    cardButton.classList.toggle('elements__card-button_type_active');
+    cardButton.classList.toggle('card__button_is-active');
   });
 });
 
 // Popup functions
 function popupEnabled() {
-  popup.classList.add('popup_type_opened')
+  popup.classList.add('popup_is-opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
 function popupDisabled() {
-  popup.classList.remove('popup_type_opened')
+  popup.classList.remove('popup_is-opened');
 }
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  profileTitle.textContent = `${nameInput.value}`;
-  profileSubtitle.textContent = `${jobInput.value}`;
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
   popupDisabled();
 }
 

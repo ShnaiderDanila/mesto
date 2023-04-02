dd073fba30a11a9535314d7118d124a6287be1f3
Shnaider Danila <shnd3r@yandex.ru>
2023-04-02 17:31:56 +0700

    feat: Добавить функцию начальной загрузки 6 карт

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const profileTitle = profile.querySelector('.profile__title');
 const profileSubtitle = profile.querySelector('.profile__subtitle');
 // Elements variables
 const elements = document.querySelector('.elements');
-const cardButtons = elements.querySelectorAll('.card__button');
+const elementsCards = elements.querySelector('.elements__cards');
 // Popup variables
 const popup = document.querySelector('.popup');
 const buttonClose = popup.querySelector('.popup__button-close');
@@ -13,12 +13,57 @@ const popupForm = popup.querySelector('.popup__form');
 const nameInput = popup.querySelector('.popup__input_type_name');
 const jobInput = popup.querySelector('.popup__input_type_job');
 
+// Elements Cards
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    alt: ''
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 // Elements functions
-cardButtons.forEach(function(cardButton) {
-  cardButton.addEventListener('click', function() {
-    cardButton.classList.toggle('card__button_is-active');
+function addStartCards() {
+  const cardTemplate = document.querySelector('#card-template').content;
+  initialCards.forEach(element => {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    card.querySelector('.card__image').src = element.link;
+    card.querySelector('.card__image').alt = element.alt;
+    card.querySelector('.card__title').textContent = element.name;
+    elementsCards.append(card);
   });
-});
+}
+addStartCards();
+
+function likeTheCard() {
+  const cardButtons = elementsCards.querySelectorAll('.card__button');
+  cardButtons.forEach(item => {
+    item.addEventListener('click', () => {
+      item.classList.toggle('card__button_is-active');
+    });
+  });
+}
+likeTheCard();
 
 // Popup functions
 function popupEnabled() {

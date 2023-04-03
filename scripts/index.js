f22777e4372425bae1ba306c8aabb6c795ce3836
Shnaider Danila <shnd3r@yandex.ru>
2023-04-03 19:55:13 +0700

    feat: Добавить функцию добавления карточек и изменить файловую структуру

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,33 @@
 // Profile variables
 const profile = document.querySelector('.profile');
 const profileButtonEdit = profile.querySelector('.profile__button-edit');
+const profileButtonAdd = profile.querySelector('.profile__button-add');
 const profileTitle = profile.querySelector('.profile__title');
 const profileSubtitle = profile.querySelector('.profile__subtitle');
 // Elements variables
 const elements = document.querySelector('.elements');
 const elementsCards = elements.querySelector('.elements__cards');
 // Popup variables
-const popup = document.querySelector('.popup');
-const buttonClose = popup.querySelector('.popup__button-close');
-const popupForm = popup.querySelector('.popup__form');
-const nameInput = popup.querySelector('.popup__input_type_name');
-const jobInput = popup.querySelector('.popup__input_type_job');
+const popupEdit = document.querySelector('.popup-edit');
+const popupEditButtonClose = popupEdit.querySelector('.popup-edit__button-close');
+const popupEditForm = popupEdit.querySelector('.popup__edit-form');
+const popupEditNameInput = popupEdit.querySelector('.popup__input_type_name');
+const popupEditJobInput = popupEdit.querySelector('.popup__input_type_job');
+
+const popupAdd = document.querySelector('.popup-add');
+const popupAddButtonClose = popupAdd.querySelector('.popup-add__button-close');
+const popupAddForm = popupAdd.querySelector('.popup__add-form');
+const popupAddPlaceNameInput = popupAdd.querySelector('.popup__input_type_place-name');
+const popupAddLinkInput = popupAdd.querySelector('.popup__input_type_link');
+
+// Card Template
+const cardTemplate = document.querySelector('#card-template').content;
 
 // Elements Cards
 const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alt: ''
   },
   {
     name: 'Челябинская область',
@@ -43,48 +52,83 @@ const initialCards = [
 ];
 
 // Elements functions
-function addStartCards() {
-  const cardTemplate = document.querySelector('#card-template').content;
-  initialCards.forEach(element => {
-    const card = cardTemplate.querySelector('.card').cloneNode(true);
-    card.querySelector('.card__image').src = element.link;
-    card.querySelector('.card__image').alt = element.alt;
-    card.querySelector('.card__title').textContent = element.name;
-    elementsCards.append(card);
-  });
-}
-addStartCards();
-
-function likeTheCard() {
-  const cardButtons = elementsCards.querySelectorAll('.card__button');
-  cardButtons.forEach(item => {
+function likeStartCards() {
+  const startCardButtons = document.querySelectorAll('.card__button');
+  startCardButtons.forEach(item => {
     item.addEventListener('click', () => {
       item.classList.toggle('card__button_is-active');
     });
   });
 }
-likeTheCard();
 
-// Popup functions
-function popupEnabled() {
-  popup.classList.add('popup_is-opened');
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
+function addStartCards () {
+  initialCards.forEach(element => {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    card.querySelector('.card__image').src = element.link;
+    card.querySelector('.card__image').alt = element.name;
+    card.querySelector('.card__title').textContent = element.name;
+    elementsCards.append(card);
+  });
+  likeStartCards();
+}
+addStartCards();
+
+// Popup Edit Functions
+function popupEditEnabled() {
+  popupEdit.classList.add('popup_is-opened');
+  popupEditNameInput.value = profileTitle.textContent;
+  popupEditJobInput.value = profileSubtitle.textContent;
 }
 
-function popupDisabled() {
-  popup.classList.remove('popup_is-opened');
+function popupEditDisabled() {
+  popupEdit.classList.remove('popup_is-opened');
 }
 
-function handleFormSubmit (evt) {
+function handleEditFormSubmit (evt) {
   evt.preventDefault();
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = jobInput.value;
-  popupDisabled();
+  profileTitle.textContent = popupEditNameInput.value;
+  profileSubtitle.textContent = popupEditJobInput.value;
+  popupEditDisabled();
 }
 
-profileButtonEdit.addEventListener('click', popupEnabled);
-buttonClose.addEventListener('click', popupDisabled);
-popupForm.addEventListener('submit', handleFormSubmit);
+profileButtonEdit.addEventListener('click', popupEditEnabled);
+popupEditButtonClose.addEventListener('click', popupEditDisabled);
+popupEditForm.addEventListener('submit', handleEditFormSubmit);
+
+// Popup Add Functions
+function popupAddEnabled() {
+  popupAdd.classList.add('popup_is-opened');
+}
+
+function popupAddDisabled() {
+  popupAdd.classList.remove('popup_is-opened');
+}
+
+function likeNewCard() {
+  const newCardButton = document.querySelector('.card__button');
+  newCardButton.addEventListener('click', () => {
+    newCardButton.classList.toggle('card__button_is-active');
+  });
+}
+
+function addNewCard() {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__image').src = popupAddLinkInput.value;
+  card.querySelector('.card__image').alt = popupAddPlaceNameInput.value;
+  card.querySelector('.card__title').textContent = popupAddPlaceNameInput.value;
+  elementsCards.prepend(card);
+  likeNewCard();
+};
+
+function handleAddFormSubmit (evt) {
+  evt.preventDefault();
+  addNewCard();
+  popupAddDisabled();
+}
+
+profileButtonAdd.addEventListener('click', popupAddEnabled);
+popupAddButtonClose.addEventListener('click', popupAddDisabled);
+popupAddForm.addEventListener('submit', handleAddFormSubmit);
+
 
 

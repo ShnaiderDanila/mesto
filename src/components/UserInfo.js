714471a7c999f9042e8e3afed7322ea435e9b180
Shnaider Danila <shnd3r@yandex.ru>
2023-06-10 14:50:29 +0700

    refactor: Изменить constructor класса UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,21 +1,26 @@
 class UserInfo {
-  constructor({usernameSelector, descriptionSelector}) {
-    this._username = document.querySelector(usernameSelector);
-    this._description = document.querySelector(descriptionSelector);
+  constructor({username, about, avatar, _id}) {
+    this._username = username;
+    this._about = about;
+    this._avatar = avatar;
+    this._userId = _id;
   }
 
 // Метод возвращения объекта с данными пользователя
   getUserInfo() {
     return {
       username: this._username.textContent,
-      description: this._description.textContent
+      about: this._about.textContent,
+      userId: this._userId
     }
   }
 
 // Метод принимает новые данные пользователя и добавляет их на страницу
-  setUserInfo({newUsername, newDescription}) {
-    this._username.textContent = newUsername;
-    this._description.textContent = newDescription;
+  setUserInfo({username, about, avatar, _id}) {
+    this._username.textContent = username;
+    this._about.textContent = about;
+    this._avatar.src = avatar;
+    this._userId = _id;
   }
 }
 
